61f848f9cfa67cd35af9e46bccbe7e21957489cd
Corey Motte <coreydavidmotte@gmail.com>
2022-02-20 14:02:04 -0500

    re-organized file structure

---
+++ script.js
@@ -0,0 +1,25 @@
+// Assignment code here
+
+function generatePassphrase() {
+  // Declaring arrays to contain each character that can be used in a password
+  var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ]
+  var characterArray = ["@", "%", "&", "(", ")", "+", "=", "#"]
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
